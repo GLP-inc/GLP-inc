4139dee9710c670ba1d321138ebb3702eedc3e66
GLP-inc <ericscrown@gmail.com>
2024-09-17 19:32:01 -0400

    change 6

--- scripts.js
+++ scripts.js
@@ -1,11 +1,2 @@
-document.addEventListener('mousemove', function(e) {
-    const topBar = document.querySelector('.top-bar');
-    const x = e.clientX / window.innerWidth;
-    const y = e.clientY / window.innerHeight;
-
-    // Chrome shifting effect for the top bar
-    const shiftX = (x * 100) - 50; // -50 to +50 range for more effect
-    const shiftY = (y * 100) - 50;
-
-    topBar.style.backgroundPosition = `${shiftX}% ${shiftY}%`;
-});
+// Removed mouse effect for the top bar
+// No JavaScript needed for this effect anymore
