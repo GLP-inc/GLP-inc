c35b3092b2936cfce3cf12317b53b7c58a8c9781
GLP-inc <ericscrown@gmail.com>
2024-09-18 20:16:06 -0400

    change 131

--- scripts.js
+++ scripts.js
@@ -1,4 +1,10 @@
 document.querySelector('.dropdown-menu').addEventListener('click', function() {
     const dropdownContent = document.querySelector('.dropdown-content');
-    dropdownContent.classList.toggle('show'); // Toggle the 'show' class
+    
+    // Toggle dropdown visibility
+    if (dropdownContent.style.display === 'block') {
+        dropdownContent.style.display = 'none';
+    } else {
+        dropdownContent.style.display = 'block';
+    }
 });
